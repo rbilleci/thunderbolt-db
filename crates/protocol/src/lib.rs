e6e7e9ecbbe381e51ad0c80114d173ac63c23bd5
rbilleci <7469913+rbilleci@users.noreply.github.com>
2026-04-25 04:54:37 +0200

    test(protocol): cover unnamed portal shared format bind

--- crates/protocol/src/lib.rs
+++ crates/protocol/src/lib.rs
@@ -6615,6 +6615,46 @@ mod tests {
             }
         );
 
+        let mut bind_unnamed_portal_with_named_statement_shared_parameter_format_payload =
+            Vec::new();
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(b"\0stmt_shared_named\0");
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&1_i16.to_be_bytes());
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&1_i16.to_be_bytes());
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&2_i16.to_be_bytes());
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&4_i32.to_be_bytes());
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(b"\x00\x00\x00\x2a");
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&(-1_i32).to_be_bytes());
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&2_i16.to_be_bytes());
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&0_i16.to_be_bytes());
+        bind_unnamed_portal_with_named_statement_shared_parameter_format_payload
+            .extend_from_slice(&1_i16.to_be_bytes());
+        let bind_unnamed_portal_with_named_statement_shared_parameter_format = frontend_frame(
+            b'B',
+            &bind_unnamed_portal_with_named_statement_shared_parameter_format_payload,
+        );
+        assert_eq!(
+            parse_frontend_message(
+                &bind_unnamed_portal_with_named_statement_shared_parameter_format,
+            )
+            .unwrap(),
+            FrontendMessage::Bind {
+                portal_name: String::new(),
+                statement_name: "stmt_shared_named".to_string(),
+                parameter_format_codes: vec![1],
+                parameters: vec![Some(vec![0x00, 0x00, 0x00, 0x2a]), None],
+                result_format_codes: vec![0, 1],
+            }
+        );
+
         let mut bind_with_shared_parameter_format_for_multiple_parameters_payload = Vec::new();
         bind_with_shared_parameter_format_for_multiple_parameters_payload
             .extend_from_slice(b"portal_shared\0stmt_shared\0");
