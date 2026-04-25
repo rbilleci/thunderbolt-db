5c4331b908527fdb13fe956eb33677e975acfe46
rbilleci <7469913+rbilleci@users.noreply.github.com>
2026-04-25 04:41:32 +0200

    test(protocol): cover unnamed binary bind

--- crates/protocol/src/lib.rs
+++ crates/protocol/src/lib.rs
@@ -4945,6 +4945,28 @@ mod tests {
             }
         );
 
+        let mut bind_unnamed_with_binary_parameter_payload = Vec::new();
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(b"\0\0");
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(&1_i16.to_be_bytes());
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(&1_i16.to_be_bytes());
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(&1_i16.to_be_bytes());
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(&4_i32.to_be_bytes());
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(&[0x00, 0xCA, 0x00, 0xFE]);
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(&1_i16.to_be_bytes());
+        bind_unnamed_with_binary_parameter_payload.extend_from_slice(&1_i16.to_be_bytes());
+        let bind_unnamed_with_binary_parameter =
+            frontend_frame(b'B', &bind_unnamed_with_binary_parameter_payload);
+        assert_eq!(
+            parse_frontend_message(&bind_unnamed_with_binary_parameter).unwrap(),
+            FrontendMessage::Bind {
+                portal_name: String::new(),
+                statement_name: String::new(),
+                parameter_format_codes: vec![1],
+                parameters: vec![Some(vec![0x00, 0xCA, 0x00, 0xFE])],
+                result_format_codes: vec![1],
+            }
+        );
+
         let mut bind_with_zero_params_and_single_shared_format_payload = Vec::new();
         bind_with_zero_params_and_single_shared_format_payload
             .extend_from_slice(b"portal3\0stmt3\0");
