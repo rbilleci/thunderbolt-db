04b60c3761042dac9bb207bb1cbeb495a0820af2
rbilleci <7469913+rbilleci@users.noreply.github.com>
2026-04-25 04:58:56 +0200

    test(protocol): cover named portal shared text bind

--- crates/protocol/src/lib.rs
+++ crates/protocol/src/lib.rs
@@ -6731,6 +6731,43 @@ mod tests {
             }
         );
 
+        let mut bind_named_portal_with_unnamed_statement_shared_text_format_payload = Vec::new();
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice("pörtal_shared_text\0\0".as_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&1_i16.to_be_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&0_i16.to_be_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&2_i16.to_be_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&("héllo".len() as i32).to_be_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice("héllo".as_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&(-1_i32).to_be_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&2_i16.to_be_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&0_i16.to_be_bytes());
+        bind_named_portal_with_unnamed_statement_shared_text_format_payload
+            .extend_from_slice(&1_i16.to_be_bytes());
+        let bind_named_portal_with_unnamed_statement_shared_text_format = frontend_frame(
+            b'B',
+            &bind_named_portal_with_unnamed_statement_shared_text_format_payload,
+        );
+        assert_eq!(
+            parse_frontend_message(&bind_named_portal_with_unnamed_statement_shared_text_format)
+                .unwrap(),
+            FrontendMessage::Bind {
+                portal_name: "pörtal_shared_text".to_string(),
+                statement_name: String::new(),
+                parameter_format_codes: vec![0],
+                parameters: vec![Some("héllo".as_bytes().to_vec()), None],
+                result_format_codes: vec![0, 1],
+            }
+        );
+
         let mut bind_with_shared_parameter_format_for_multiple_parameters_payload = Vec::new();
         bind_with_shared_parameter_format_for_multiple_parameters_payload
             .extend_from_slice(b"portal_shared\0stmt_shared\0");
